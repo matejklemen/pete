931b2c4702d0d1b07787ff7333fcf730405d09a6
Matej Klemen <matej.klemen1337@gmail.com>
2020-10-07 19:59:56 +0200

    highlight.js: Fix scaling of visualization from [min, max] -> [-1, 1] to [-maxAbs, maxAbs] -> [-1, 1]; add simple tooltips displaying unscaled feature importances

--- explain_nlp/visualizations/highlight.js
+++ explain_nlp/visualizations/highlight.js
@@ -5,19 +5,25 @@ function visualizeExample(sequence, label, importances) {
 	let minImportance = importances.reduce((currMin, currValue) => currValue < currMin? currValue: currMin);
 	let maxImportance = importances.reduce((currMax, currValue) => currValue > currMax? currValue: currMax);
 
-	// scale importances to [-1, 1] range
-	let scaledImportances = importances.map(imp => (1 - (-1)) * (imp - minImportance) / (maxImportance - minImportance) + (-1));
+	let maxAbsImportance = Math.max(Math.abs(minImportance), Math.abs(maxImportance))
+	let bottomLimit = - maxAbsImportance;
+	let topLimit = maxAbsImportance;
+
+	// scale importances to [-maxAbsImportance, maxAbsImportance] range
+	let scaledImportances = importances.map(imp => (1 - (-1)) * (imp - bottomLimit) / (topLimit - bottomLimit) + (-1));
 
 	let createdSpans = scaledImportances.map((imp, i) => {
+		let roundedOrigImportance = importances[i].toFixed(3);
+
 		if(imp < 0) {
-			return `<span style="background-color: rgba(255, 0, 0, ${Math.abs(imp)})">${sequence[i]}</span>`
+			return `<span title="${roundedOrigImportance}" style="background-color: rgba(255, 0, 0, ${Math.abs(imp)})">${sequence[i]}</span>`
 		}
 		else if (imp > 0) {
-			return `<span style="background-color: rgba(0, 153, 0, ${imp})">${sequence[i]}</span>`
+			return `<span title="${roundedOrigImportance}" style="background-color: rgba(0, 153, 0, ${imp})">${sequence[i]}</span>`
 		}
 		// leave 0 importance uncolored
 		else
-			return `<span>${sequence[i]}</span>`
+			return `<span title="${roundedOrigImportance}">${sequence[i]}</span>`
 	});
-	examples_div.innerHTML += `<div class="example" style="word-break: break-word"><span class="example-label">Label: ${label}</span> <br />${createdSpans.join("")}</div>`;
+	examples_div.innerHTML += `<div class="example" style="word-break: break-word"><span class="example-label">Predicted label: ${label}</span> <br />${createdSpans.join("")}</div>`;
 }
