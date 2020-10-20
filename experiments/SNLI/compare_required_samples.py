af9bba59e846a2aee1811194b2d0908b1fa8282a
Matej Klemen <matej.klemen1337@gmail.com>
2020-10-20 17:24:17 +0200

    Fix SNLI required samples comparison to work with refactored logic

--- experiments/SNLI/compare_required_samples.py
+++ experiments/SNLI/compare_required_samples.py
@@ -1,19 +1,19 @@
 import argparse
 import json
-from time import time
 import os
+from time import time
 
 import pandas as pd
 import torch
 from torch.utils.data import Dataset, DataLoader
-from transformers import BertTokenizer, BertForSequenceClassification
 
+from explain_nlp.methods.generation import BertForMaskedLMGenerator
 from explain_nlp.methods.ime import IMEExplainer
 from explain_nlp.methods.ime_mlm import IMEMaskedLMExplainer
+from explain_nlp.methods.modeling import InterpretableBertForSequenceClassification
 from explain_nlp.methods.utils import estimate_max_samples
 from explain_nlp.visualizations.highlight import highlight_plot
 
-DEVICE = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 EXPERIMENT_DESCRIPTION = \
 """Compare estimated number of required samples (IME vs IME+MLM) to satisfy theoretical 'guarantees' on max AE 
 specified by `confidence_interval` and `max_abs_error`. Ran on SNLI. See config.json for specific options."""
@@ -27,6 +27,12 @@ parser.add_argument("--generator_dir", type=str, default="bert-base-uncased",
 parser.add_argument("--max_seq_len", type=int, default=41)
 parser.add_argument("--generator_batch_size", type=int, default=8)
 parser.add_argument("--model_batch_size", type=int, default=8)
+parser.add_argument("--top_p", type=float, default=None)
+parser.add_argument("--masked_at_once", type=float, default=None,
+                    help="Proportion of tokens to mask out at once during masked language modeling. By default, "
+                         "mask out one token at a time")
+parser.add_argument("--p_ensure_different", type=float, default=0.0,
+                    help="Probability of forcing a generated token to be different from the token in given data")
 
 parser.add_argument("--num_generated_samples", type=int, default=100)
 parser.add_argument("--min_samples_per_feature", type=int, default=10,
@@ -40,6 +46,8 @@ parser.add_argument("--return_model_scores", action="store_true")
 parser.add_argument("--experiment_dir", type=str, default="debug")
 parser.add_argument("--save_every_n_examples", type=int, default=5,
                     help="Save experiment data every N examples in order to avoid losing data on longer computations")
+
+parser.add_argument("--use_cpu", action="store_true", help="Use CPU instead of GPU")
 
 
 class NLIDataset(Dataset):
@@ -97,6 +105,8 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     alpha = 1 - args.confidence_interval
+    masked_at_once = args.masked_at_once if args.masked_at_once is not None else 1
+    DEVICE = torch.device("cpu") if args.use_cpu else torch.device("cuda")
 
     experiment_dir = args.experiment_dir
     if experiment_dir is None:
@@ -113,92 +123,86 @@ if __name__ == "__main__":
     with open(os.path.join(experiment_dir, "README.txt"), "w") as f_config:
         print(EXPERIMENT_DESCRIPTION, file=f_config)
 
-    tokenizer = BertTokenizer.from_pretrained(args.model_dir)
-    model = BertForSequenceClassification.from_pretrained(args.model_dir).to(DEVICE)
+    model = InterpretableBertForSequenceClassification(tokenizer_name=args.model_dir,
+                                                       model_name=args.model_dir,
+                                                       batch_size=args.model_batch_size,
+                                                       max_seq_len=args.max_seq_len,
+                                                       device="cpu" if args.use_cpu else "cuda")
+    generator = BertForMaskedLMGenerator(tokenizer_name=args.generator_dir,
+                                         model_name=args.generator_dir,
+                                         batch_size=args.generator_batch_size,
+                                         max_seq_len=args.max_seq_len,
+                                         device="cpu" if args.use_cpu else "cuda",
+                                         top_p=args.top_p,
+                                         masked_at_once=masked_at_once,
+                                         p_ensure_different=args.p_ensure_different)
 
-    df_train = load_nli(args.train_path).sample(frac=1.0).reset_index(drop=True)
-    df_test = load_nli(args.test_path)
+    df_train = load_nli(args.train_path, sample_size=10).sample(frac=1.0).reset_index(drop=True)
+    df_test = load_nli(args.test_path, sample_size=5)
     train_set = NLIDataset(premises=df_train["sentence1"].values,
                            hypotheses=df_train["sentence2"].values,
                            labels=df_train["gold_label"].apply(lambda label_str: LABEL_TO_IDX[label_str]).values,
-                           tokenizer=tokenizer,
+                           tokenizer=model.tokenizer,
                            max_seq_len=args.max_seq_len)
     test_set = NLIDataset(premises=df_test["sentence1"].values,
                           hypotheses=df_test["sentence2"].values,
                           labels=df_test["gold_label"].apply(lambda label_str: LABEL_TO_IDX[label_str]).values,
-                          tokenizer=tokenizer,
+                          tokenizer=model.tokenizer,
                           max_seq_len=args.max_seq_len)
 
-    ime = IMEExplainer(sample_data=train_set.input_ids, return_scores=args.return_model_scores, return_num_samples=True,
+    ime = IMEExplainer(sample_data=train_set.input_ids, model=model,
+                       return_scores=args.return_model_scores, return_num_samples=True,
                        return_samples=args.return_generated_samples, return_variance=True)
-    ime_mlm = IMEMaskedLMExplainer(pretrained_name_or_path=args.generator_dir, batch_size=args.generator_batch_size,
+    ime_mlm = IMEMaskedLMExplainer(model=model, generator=generator, num_generated_samples=args.num_generated_samples,
                                    return_scores=args.return_model_scores, return_num_samples=True,
-                                   return_samples=args.return_generated_samples, return_variance=True,
-                                   num_generated_samples=args.num_generated_samples)
+                                   return_samples=args.return_generated_samples, return_variance=True)
 
     examples_log = []
-    ime_importances = []
-    ime_mlm_importances = []
+    ime_importances, ime_mlm_importances = [], []
     sequences, labels = [], []
+
+    # If directory exists, try loading existing data
+    if os.path.exists(os.path.join(experiment_dir, "examples.json")):
+        with open(os.path.join(experiment_dir, "examples.json")) as f:
+            examples_log = json.load(f)
+        ime_importances = [ex["ime_data"]["importance"] for ex in examples_log]
+        ime_mlm_importances = [ex["ime_mlm_data"]["importance"] for ex in examples_log]
+        sequences = [ex["sequence"] for ex in examples_log]
+        labels = [ex["predicted_label"] for ex in examples_log]
+
+        print(f"Loaded data for {len(examples_log)} existing examples!")
+
     for idx_example, curr_example in enumerate(DataLoader(test_set, batch_size=1, shuffle=False)):
-        @torch.no_grad()
-        def _model_wrapper(data: torch.Tensor):
-            num_examples = data.shape[0]
-            batch_size = min(args.model_batch_size, num_examples)
-            aux_inputs = {
-                "token_type_ids": curr_example["token_type_ids"].repeat_interleave(batch_size, dim=0).to(DEVICE),
-                "attention_mask": curr_example["attention_mask"].repeat_interleave(batch_size, dim=0).to(DEVICE)
-            }
-
-            num_batches = (num_examples + batch_size - 1) // batch_size
-            all_scores = []
-            for idx_batch in range(num_batches):
-                input_ids = data[idx_batch * batch_size: (idx_batch + 1) * batch_size]
-                curr_batch_size = input_ids.shape[0]
-                output = model(input_ids=input_ids.to(DEVICE),
-                               token_type_ids=aux_inputs["token_type_ids"][: curr_batch_size],
-                               attention_mask=aux_inputs["attention_mask"][: curr_batch_size],
-                               return_dict=True)
-                all_scores.append(torch.nn.functional.softmax(output["logits"], dim=-1))
-
-            return torch.cat(all_scores, dim=0)
-
-
         _curr_example = {k: v.to(DEVICE) for k, v in curr_example.items() if k not in {"labels", "special_tokens_mask"}}
-        probas = _model_wrapper(_curr_example["input_ids"])
-        predicted_label = int(torch.argmax(probas))
+        probas = model.score(**_curr_example)
+        predicted_label = int(torch.argmax(probas, dim=-1))
         actual_label = int(curr_example["labels"])
 
         t1 = time()
-        ime_res = ime.explain(curr_example["input_ids"],
-                              label=predicted_label,
-                              min_samples_per_feature=args.min_samples_per_feature,
-                              model_func=_model_wrapper,
-                              perturbable_mask=torch.logical_not(curr_example["special_tokens_mask"]))
+        ime_res = ime.explain_text((df_test.iloc[idx_example]["sentence1"],
+                                    df_test.iloc[idx_example]["sentence2"]),
+                                   label=predicted_label,
+                                   min_samples_per_feature=args.min_samples_per_feature)
         t2 = time()
 
-        ime_est_samples = int(estimate_max_samples(ime_res["var"] * args.min_samples_per_feature, alpha=alpha,
-                                                   max_abs_error=args.max_abs_error))
+        ime_est_samples = int(estimate_max_samples(ime_res["var"] * ime_res["num_samples"],
+                                                   alpha=alpha, max_abs_error=args.max_abs_error))
         print(f"[IME] Estimated samples required: {ime_est_samples}")
-        print(f"[IME] Time taken: {t2 - t1}")
+        print(f"[IME] Time taken: {t2 - t1: .3f}s")
 
         t3 = time()
-        ime_mlm_res = ime_mlm.explain(curr_example["input_ids"],
-                                      label=predicted_label,
-                                      min_samples_per_feature=args.min_samples_per_feature,
-                                      model_func=_model_wrapper,
-                                      perturbable_mask=torch.logical_not(curr_example["special_tokens_mask"]),
-                                      token_type_ids=curr_example["token_type_ids"],
-                                      attention_mask=curr_example["attention_mask"])
+        ime_mlm_res = ime_mlm.explain_text((df_test.iloc[idx_example]["sentence1"],
+                                            df_test.iloc[idx_example]["sentence2"]),
+                                           label=predicted_label,
+                                           min_samples_per_feature=args.min_samples_per_feature)
         t4 = time()
 
-        ime_mlm_est_samples = int(estimate_max_samples(ime_mlm_res["var"] * args.min_samples_per_feature,
-                                                       alpha=alpha,
-                                                       max_abs_error=args.max_abs_error))
+        ime_mlm_est_samples = int(estimate_max_samples(ime_mlm_res["var"] * ime_mlm_res["num_samples"],
+                                                       alpha=alpha, max_abs_error=args.max_abs_error))
         print(f"[IME MLM] Estimated samples required: {ime_mlm_est_samples}")
-        print(f"[IME MLM] Time taken: {t4 - t3}")
+        print(f"[IME MLM] Time taken: {t4 - t3: .3f}s")
 
-        sequence_tokens = tokenizer.convert_ids_to_tokens(curr_example["input_ids"][0])  # type: list
+        sequence_tokens = ime_mlm_res["input"]
 
         ime_gen_samples, ime_mlm_gen_samples = [], []
         if args.return_generated_samples:
@@ -206,15 +210,13 @@ if __name__ == "__main__":
                 if curr_samples is None:  # non-perturbable feature
                     ime_gen_samples.append([])
                 else:
-                    ime_gen_samples.append([tokenizer.convert_ids_to_tokens(sample)
-                                            for sample in curr_samples.tolist()])
+                    ime_gen_samples.append(ime.model.convert_ids_to_tokens(curr_samples))
 
             for curr_samples in ime_mlm_res["samples"]:
                 if curr_samples is None:  # non-perturbable feature
                     ime_mlm_gen_samples.append([])
                 else:
-                    ime_mlm_gen_samples.append([tokenizer.convert_ids_to_tokens(sample)
-                                                for sample in curr_samples.tolist()])
+                    ime_mlm_gen_samples.append(ime_mlm.model.convert_ids_to_tokens(curr_samples))
 
         ime_data = {
             "importance": ime_res["importance"].tolist(),
