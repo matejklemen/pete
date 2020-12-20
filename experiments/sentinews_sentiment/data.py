5fc659ffa880a86fb0db7406369d620ec6d5ae23
Matej Klemen <matej.klemen1337@gmail.com>
2020-12-20 18:53:02 +0100

    Add script for computing accurate importances for SentiNews Slovene sentiment classification

---
+++ experiments/sentinews_sentiment/data.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import torch
+from torch.utils.data import Dataset
+
+LABEL_TO_IDX = {"neutral": 0, "negative": 1, "positive": 2}
+IDX_TO_LABEL = {i: label for label, i in LABEL_TO_IDX.items()}
+
+
+class SequenceDataset(Dataset):
+    def __init__(self, content, labels, tokenizer, max_seq_len=128):
+        self.input_ids = []
+        self.segments = []
+        self.attn_masks = []
+        self.special_tokens_masks = []
+        self.labels = []
+        self.max_seq_len = max_seq_len
+
+        for curr_content, curr_label in zip(content, labels):
+            processed = tokenizer.encode_plus(curr_content, max_length=max_seq_len,
+                                              padding="max_length", truncation="longest_first",
+                                              return_special_tokens_mask=True)
+            self.input_ids.append(processed["input_ids"])
+            self.segments.append(processed["token_type_ids"])
+            self.attn_masks.append(processed["attention_mask"])
+            self.special_tokens_masks.append(processed["special_tokens_mask"])
+            self.labels.append(curr_label)
+
+        self.input_ids = torch.tensor(self.input_ids)
+        self.segments = torch.tensor(self.segments)
+        self.attn_masks = torch.tensor(self.attn_masks)
+        self.special_tokens_masks = torch.tensor(self.special_tokens_masks)
+        self.labels = torch.tensor(self.labels)
+
+    def __getitem__(self, idx):
+        return {
+            "input_ids": self.input_ids[idx],
+            "token_type_ids": self.segments[idx],
+            "attention_mask": self.attn_masks[idx],
+            "special_tokens_mask": self.special_tokens_masks[idx],
+            "labels": self.labels[idx]
+        }
+
+    def __len__(self):
+        return self.input_ids.shape[0]
+
+def load_sentinews(file_path, sample_size=None):
+    return pd.read_csv(file_path, sep="\t", nrows=sample_size)
