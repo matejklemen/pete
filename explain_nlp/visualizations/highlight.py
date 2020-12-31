65f12f135b05b1c2fe8a2be79d8aa8ee1c26bcd1
Matej Klemen <matej.klemen1337@gmail.com>
2020-12-31 11:25:41 +0100

    Note down custom feature type (if any) in MethodData, tweak visualization for bigger units

--- explain_nlp/visualizations/highlight.py
+++ explain_nlp/visualizations/highlight.py
@@ -1,4 +1,5 @@
 import os
+from itertools import groupby
 from typing import List, Optional, Mapping
 import numpy as np
 from explain_nlp.experimental.core import MethodData
@@ -59,8 +60,10 @@ def highlight_plot(sequences: List[List[str]],
                    importances: List[List[float]],
                    pred_labels: List,
                    actual_labels: Optional[List] = None,
+                   custom_features: Optional[List[List[List[int]]]] = None,  # list of feature groups per example
                    path: Optional[str] = None):
     eff_actual_labels = [None for _ in range(len(sequences))] if actual_labels is None else actual_labels
+    eff_custom_features = [[] for _ in range(len(sequences))] if custom_features is None else custom_features
 
     if len(sequences) != len(importances):
         raise ValueError(f"Got an unequal amount of sequences and importances of sequence elements "
@@ -70,18 +73,56 @@ def highlight_plot(sequences: List[List[str]],
         raise ValueError(f"Got an unequal amount of sequences and labels "
                          f"({len(sequences)} sequences != {len(pred_labels)} labels)")
 
-    np_importances = np.array(importances)
-    max_abs_imps = np.max(np.abs(np_importances), axis=1, keepdims=True)
-    scaled_imps = scale_interval(np_importances, curr_low=-max_abs_imps, curr_high=max_abs_imps)
+    if len(sequences) != len(eff_custom_features):
+        raise ValueError(f"Got an unequal amount of sequences and custom feature groups "
+                         f"({len(sequences)} sequences != feature groups for {len(eff_custom_features)} examples)")
+
+    np_importances, scaled_imps = [], []
+    for i, curr_imps in enumerate(importances):
+        # If custom features are provided, only use those importances for normalization
+        idx_start_importance = len(sequences[i]) if len(eff_custom_features[i]) > 0 else 0
+        curr_np = np.array(curr_imps)
+        np_importances.append(curr_np)
+        max_abs_imp = np.max(np.abs(curr_np[idx_start_importance:]))
+        scaled_imps.append(scale_interval(curr_np, curr_low=-max_abs_imp, curr_high=max_abs_imp))
 
     body_html = []
-    for i, (curr_seq, curr_sc_imps, curr_pred_label, curr_actual_label) in enumerate(zip(sequences,
-                                                                                         scaled_imps,
-                                                                                         pred_labels,
-                                                                                         eff_actual_labels)):
-        if len(curr_seq) != len(curr_sc_imps):
-            raise ValueError(f"Example #{i}: importance not provided for each sequence element "
-                             f"({len(curr_seq)} sequence elements != {len(curr_sc_imps)} )")
+    for i, (curr_seq, curr_pred_label, curr_actual_label, curr_custom_features) in enumerate(zip(sequences,
+                                                                                                 pred_labels,
+                                                                                                 eff_actual_labels,
+                                                                                                 eff_custom_features)):
+        curr_sc_imps = scaled_imps[i]
+        if len(curr_seq) + len(curr_custom_features) != len(curr_sc_imps):
+            raise ValueError(f"Example #{i}: importance not provided for each feature (group) "
+                             f"({len(curr_seq) + len(curr_custom_features)} features != "
+                             f"{len(curr_sc_imps)} importances)")
+
+        relevant_features = curr_custom_features if len(curr_custom_features) > 0 else [[i] for i in range(len(curr_seq))]
+        is_covered = np.zeros(len(curr_seq))
+        for curr_group in relevant_features:
+            if np.any(is_covered[curr_group]):
+                raise ValueError(f"Features are not allowed to have multiple importances (feature group {curr_group} "
+                                 f"overlaps with some other group)")
+
+        idx_start_importance = len(curr_seq) if len(curr_custom_features) > 0 else 0
+        # Some tokens may be grouped and therefore share the importance, stored at a custom index
+        token_to_importance = np.arange(len(curr_seq))
+        is_start = np.zeros(len(curr_seq), dtype=bool)
+        is_end = np.zeros(len(curr_seq), dtype=bool)
+        is_multiunit = np.zeros(len(curr_seq), dtype=bool)
+
+        for idx_importance, curr_group in enumerate(relevant_features, start=idx_start_importance):
+            # Feature groups might not be contiguous, so find out the beginnings and endings of their parts
+            # e.g. group [1, 2, 7, 8] will be visualized as spans [1, 2] and [7, 8]
+            for _, g in groupby(enumerate(sorted(curr_group)), lambda x: x[0] - x[1]):
+                cont_features = list(map(lambda tup: tup[1], g))
+                is_start[cont_features[0]] = True
+                is_end[cont_features[-1]] = True
+                if len(curr_group) > 1:
+                    is_multiunit[cont_features[0]] = True
+
+            for curr_feature in curr_group:
+                token_to_importance[curr_feature] = idx_importance
 
         curr_ex_html = []
         # If label is not given, color it with yellow, indicating "unknown correctness"
@@ -98,18 +139,32 @@ def highlight_plot(sequences: List[List[str]],
                             f"</span>")
         curr_ex_html.append("<br />")
 
-        for idx_tok, scaled_imp in enumerate(curr_sc_imps):
+        for idx_tok, curr_tok in enumerate(curr_seq):
+            unscaled_imp = np_importances[i][token_to_importance[idx_tok]]
+            scaled_imp = curr_sc_imps[token_to_importance[idx_tok]]
             # Most green and most red colors indicate highest and (-highest) importances (i.e. equal tails)
             curr_color = f"rgba(0, 153, 0, {scaled_imp})" if scaled_imp > 0 else f"rgba(255, 0, 0, {abs(scaled_imp)})"
 
-            curr_ex_html.append(f"<span class='example-unit' "
-                                f"id='token-{i}-{idx_tok}'"
-                                f"title='{np_importances[i, idx_tok]: .4f}' "
-                                f"style='background-color: {curr_color}'>"
-                                f"{curr_seq[idx_tok]}"
-                                f"</span>")
+            tok_parts = []
+            if is_start[idx_tok]:
+                tok_parts.append(f"<span class='example-unit' "
+                                 f"title='{unscaled_imp: .4f}' "
+                                 f"style='background-color: {curr_color}'"
+                                 f"data-sequence='{i}' data-feature='{token_to_importance[idx_tok]}' "
+                                 f"onmouseenter='toggleMarked(this)' "
+                                 f"onmouseleave='toggleMarked(this)'>")
+                # Display an ID in front of (possibly scattered) units making up large, discontiguous unit
+                if is_multiunit[idx_tok]:
+                    tok_parts.append(f"<span class='unit-id'>#{token_to_importance[idx_tok]}</span>")
 
-        body_html.append("<div class='example'>{}</div>".format("\n".join(curr_ex_html)))
+            tok_parts.append(curr_tok)
+
+            if is_end[idx_tok]:
+                tok_parts.append(f"</span>")
+
+            curr_ex_html.append("".join(tok_parts))
+
+        body_html.append("<div class='example' id='{}'>{}</div>".format(i, "\n".join(curr_ex_html)))
 
     body_html = "\n".join(body_html)
     return base_visualization(body_html, path=path)
@@ -119,8 +174,12 @@ def highlight_plot_multiple_methods(sequences: List[List[str]],
                                     importances: Mapping[str, List[List[float]]],
                                     pred_labels: List,
                                     actual_labels: Optional[List] = None,
+                                    custom_features: Optional[Mapping[str, List[List[List[int]]]]] = None,
                                     path: Optional[str] = None):
     eff_actual_labels = [None for _ in range(len(sequences))] if actual_labels is None else actual_labels
+    eff_custom_features = {m: [[] for _ in range(len(sequences))] for m in importances.keys()} \
+        if custom_features is None else custom_features
+
     for method_name, method_imps in importances.items():
         if len(sequences) != len(method_imps):
             raise ValueError(f"Got an unequal amount of sequences and importances of sequence elements "
@@ -130,66 +189,39 @@ def highlight_plot_multiple_methods(sequences: List[List[str]],
         raise ValueError(f"Got an unequal amount of sequences and labels "
                          f"({len(sequences)} sequences != {len(pred_labels)} labels)")
 
-    np_importances = {method_name: np.array(method_imps) for method_name, method_imps in importances.items()}
-    max_abs_imps = {method_name: np.max(np.abs(method_imps), axis=1, keepdims=True)
-                    for method_name, method_imps in np_importances.items()}
-    scaled_imps = {method_name: scale_interval(np_importances[method_name],
-                                               curr_low=-max_abs_imps[method_name],
-                                               curr_high=max_abs_imps[method_name])
-                   for method_name in importances}
+    flat_sequences, flat_importances = [], []
+    flat_pred, flat_actual = [], []
+    flat_custom_features = []
 
     method_order = list(importances.keys())
-    body_html = []
     for i, (curr_seq, curr_pred_label, curr_actual_label) in enumerate(zip(sequences, pred_labels, eff_actual_labels)):
-        curr_ex_html = []
-
-        # If label is not given, color it with yellow, indicating "unknown correctness"
-        label_color = "rgb(238, 232, 170)"
-        eff_actual_label = "/"
-        if curr_actual_label is not None:
-            label_color = "rgba(200, 247, 197, 1)" if curr_actual_label == curr_pred_label else "rgba(236, 100, 75, 1)"
-            eff_actual_label = curr_actual_label
-
-        curr_ex_html.append(f"<span class='example-label' "
-                            f"title='Actual label: {eff_actual_label}' "
-                            f"style='background-color: {label_color}'> "
-                            f"Predicted label: {curr_pred_label} "
-                            f"</span>")
-        curr_ex_html.append("<br />")
-
         for method_name in method_order:
-            method_html = []
-            method_html.append(f"<span class='method-name'>{method_name}</span>")
-            method_html.append("<br />")
+            curr_imps = importances[method_name][i]
+            curr_custom_features = eff_custom_features[method_name][i]
 
-            curr_sc_imps = scaled_imps[method_name][i]
-            if len(curr_seq) != len(curr_sc_imps):
-                raise ValueError(f"Example #{i}: importance not provided for each sequence element"
-                                 f"({len(curr_seq)} sequence elements != {len(curr_sc_imps)} in method '{method_name}')")
+            if len(curr_seq) + len(curr_custom_features) != len(curr_imps):
+                raise ValueError(f"Example #{i}: importance not provided for each feature (group) "
+                                 f"({len(curr_seq) + len(curr_custom_features)} features != "
+                                 f"{len(curr_imps)} importances in method '{method_name}')")
 
-            for idx_tok, scaled_imp in enumerate(curr_sc_imps):
-                # Most green and most red colors indicate highest and (-highest) importances (i.e. equal tails)
-                curr_color = f"rgba(0, 153, 0, {scaled_imp})" if scaled_imp > 0 else f"rgba(255, 0, 0, {abs(scaled_imp)})"
+            flat_sequences.append(curr_seq)
+            flat_importances.append(curr_imps)
+            flat_pred.append(f"{curr_pred_label} (<b>{method_name}</b>)")
+            flat_actual.append(f"{'/' if curr_actual_label is None else curr_actual_label} (<b>{method_name}</b>)")
+            flat_custom_features.append(curr_custom_features)
 
-                method_html.append(f"<span class='example-unit' "
-                                   f"id='token-{i}-{idx_tok}'"
-                                   f"title='{np_importances[method_name][i, idx_tok]: .4f}' "
-                                   f"style='background-color: {curr_color}'>"
-                                   f"{curr_seq[idx_tok]}"
-                                   f"</span>")
-
-            curr_ex_html.append("<div class='method'>{}</div>".format("\n".join(method_html)))
-
-        body_html.append("<div class='example'>{}</div>".format("\n".join(curr_ex_html)))
-
-    body_html = "\n".join(body_html)
-    return base_visualization(body_html, path=path)
+    return highlight_plot(sequences=flat_sequences, importances=flat_importances,
+                          pred_labels=flat_pred, actual_labels=flat_actual,
+                          custom_features=flat_custom_features, path=path)
 
 
 def track_progress(method_data: MethodData, idx_example: int,
                    path: Optional[str] = None, track_every_n_steps=100):
     """ Reconstructing the construction of explanation. In a nutshell, this is a simplistic reimplementation of IME
-    with added HTML construction. """
+        with added HTML construction.
+        TODO: raise NotImplementedError when custom features are used
+        TODO: add support for custom features
+    """
     sequence = method_data.sequences[idx_example]
     predicted_label = method_data.pred_labels[idx_example]
     actual_label = method_data.actual_labels[idx_example]
@@ -391,12 +423,25 @@ def track_progress(method_data: MethodData, idx_example: int,
 
 
 if __name__ == "__main__":
-    # highlight_plot(sequences=[["The", "show", "was", "fucking", "shite", "but", "I", "liked", "the", "end"]],
-    #                importances=[[0.0, -0.05, 0.0, 0.35, 0.05, 0.01, 0.0, -0.1, 0.0, -0.05]],
-    #                pred_labels=["toxic"], actual_labels=["toxic"], path="tmp_refactor.html")
-
-    ime_run1 = MethodData.load("/home/matej/Downloads/SNLI_first_ex_comparison/snli_xs_ex0_accurate_ime/ime_data.json")
-    track_progress(method_data=ime_run1,
-                   idx_example=0,
+    highlight_plot(sequences=[["The", "show", "was", "fucking", "shite", "but", "I", "liked", "the", "end"],
+                              ["The", "big", "brown", "fox", "jumps", "over", "a", "lazy", "dog"]],
+                   importances=[[0.0, -0.05, 0.0, 0.35, 0.05, 0.01, 0.0, -0.1, 0.0, -0.05, 0.45, -0.2],
+                                [0.02, 0.2, 0.1, 0.1, 0.05, 0.05, 0.01, -0.1, 0.1, 0.3, -0.15]],
+                   pred_labels=["toxic", "clean"],
+                   actual_labels=["toxic", None],
+                   custom_features=[[[3, 4, 9], [7]],
+                                    [[1, 2, 3], [7, 8]]],
                    path="tmp_refactor.html")
+
+    # data = MethodData.load("/home/matej/Downloads/imdb_xs_ime_accurate_values_r1/ime_data.json")
+    # highlight_plot(data.sequences,
+    #                data.importances,
+    #                data.pred_labels,
+    #                data.actual_labels,
+    #                path="tmp_refactor.html")
+
+    # ime_run1 = MethodData.load("/home/matej/Downloads/SNLI_first_ex_comparison/snli_xs_ex0_accurate_ime/ime_data.json")
+    # track_progress(method_data=ime_run1,
+    #                idx_example=0,
+    #                path="tmp_refactor.html")
 
