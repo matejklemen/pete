f74981c63a566251c78be762d54ba441c80e81ef
Matej Klemen <matej.klemen1337@gmail.com>
2021-01-04 22:20:07 +0100

    Clean up sentinews experiment script

--- explain_nlp/experimental/data.py
+++ explain_nlp/experimental/data.py
@@ -5,7 +5,8 @@ import torch
 from torch.utils.data import Dataset
 
 LABEL_TO_IDX = {
-    "snli": {"entailment": 0, "neutral": 1, "contradiction": 2}
+    "snli": {"entailment": 0, "neutral": 1, "contradiction": 2},
+    "sentinews": {"neutral": 0, "negative": 1, "positive": 2}
 }
 IDX_TO_LABEL = {dataset: {i: lbl for lbl, i in label_mapping.items()}
                 for dataset, label_mapping in LABEL_TO_IDX.items()}
@@ -50,6 +51,44 @@ class TransformerSeqPairDataset(Dataset):
         return self.input_ids.shape[0]
 
 
+class TransformerSeqDataset(Dataset):
+    def __init__(self, sequences: Iterable[str], labels, tokenizer, max_seq_len=128):
+        self.input_ids = []
+        self.segments = []
+        self.attn_masks = []
+        self.special_tokens_masks = []
+        self.labels = []
+        self.max_seq_len = max_seq_len
+
+        for curr_seq, curr_label in zip(sequences, labels):
+            processed = tokenizer.encode_plus(curr_seq, max_length=max_seq_len,
+                                              padding="max_length", truncation="longest_first",
+                                              return_special_tokens_mask=True)
+            self.input_ids.append(processed["input_ids"])
+            self.segments.append(processed["token_type_ids"])
+            self.attn_masks.append(processed["attention_mask"])
+            self.special_tokens_masks.append(processed["special_tokens_mask"])
+            self.labels.append(curr_label)
+
+        self.input_ids = torch.tensor(self.input_ids)
+        self.segments = torch.tensor(self.segments)
+        self.attn_masks = torch.tensor(self.attn_masks)
+        self.special_tokens_masks = torch.tensor(self.special_tokens_masks)
+        self.labels = torch.tensor(self.labels)
+
+    def __getitem__(self, idx):
+        return {
+            "input_ids": self.input_ids[idx],
+            "token_type_ids": self.segments[idx],
+            "attention_mask": self.attn_masks[idx],
+            "special_tokens_mask": self.special_tokens_masks[idx],
+            "labels": self.labels[idx]
+        }
+
+    def __len__(self):
+        return self.input_ids.shape[0]
+
+
 def load_nli(file_path, sample_size=None):
     """ Common loader for SNLI/MultiNLI """
     df = pd.read_csv(file_path, sep="\t", na_values=[""], nrows=sample_size, encoding="utf-8", quoting=3)
@@ -59,3 +98,6 @@ def load_nli(file_path, sample_size=None):
     df = df.loc[mask].reset_index(drop=True)
 
     return df
+
+def load_sentinews(file_path, sample_size=None):
+    return pd.read_csv(file_path, sep="\t", nrows=sample_size)
