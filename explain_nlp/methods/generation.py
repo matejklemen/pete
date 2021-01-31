49761e9d08572eebf7a0b3be582f80b3188207fd
Matej Klemen <matej.klemen1337@gmail.com>
2021-01-31 17:08:07 +0100

    Remove some leftover prints in generation

--- explain_nlp/methods/generation.py
+++ explain_nlp/methods/generation.py
@@ -223,8 +223,6 @@ class GPTLMGenerator(SampleGenerator):
                                 **generation_kwargs):
         num_samples = generation_mask.shape[0]
         num_features = input_ids.shape[1]
-        print("Instance:")
-        print(self.tokenizer.convert_ids_to_tokens(input_ids[0]))
 
         original_input_values = input_ids[0].clone()
         eff_input_ids = input_ids
@@ -355,8 +353,6 @@ class GPTControlledLMGenerator(GPTLMGenerator):
                                 generation_mask: torch.Tensor,
                                 idx_observed_feature,
                                 **generation_kwargs):
-        print("CONTROLLED!")
-
         num_samples = generation_mask.shape[0]
         eff_input_ids = extend_tensor(input_ids)
         if input_ids.shape[0] != num_samples:
