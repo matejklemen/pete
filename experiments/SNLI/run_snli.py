Fix LIME for higher units, allow using LIME in SNLI experiment

experiments/SNLI/run_snli.py
@@ -96,7 +96,7 @@ if __name__ == "__main__":
                                          # Method-specific options below:
                                          used_sample_data=used_sample_data, generator=generator,
                                          num_generated_samples=args.num_generated_samples,
-                                         data_weights=data_weights)
+                                         data_weights=data_weights, kernel_width=args.kernel_width)
 
     # Container that wraps debugging data and a lot of repetitive appends
     method_data = MethodData(method_type=method_type, model_description=model_description,
@@ -145,27 +145,38 @@ if __name__ == "__main__":
                                             pipe=nlp)
             pretokenized_example = pretokenized_test_data[idx_example]
 
+        side_results = {}
         t1 = time()
-        res = method.explain_text(input_pair, pretokenized_text_data=pretokenized_example,
-                                  label=predicted_label, min_samples_per_feature=args.min_samples_per_feature,
-                                  custom_features=curr_features)
-        t2 = time()
-
-        if compute_accurately:
-            taken_or_estimated_samples = res['taken_samples']
+        if "lime" in args.method:
+            res = method.explain_text(input_pair, pretokenized_text_data=pretokenized_example,
+                                      label=predicted_label, num_samples=args.min_samples_per_feature,
+                                      explanation_length=args.explanation_length,
+                                      custom_features=curr_features)
+            t2 = time()
         else:
-            required_samples_per_feature = estimate_feature_samples(res["var"] * res["num_samples"],
-                                                                    alpha=(1 - args.confidence_interval),
-                                                                    max_abs_error=args.max_abs_error)
-            required_samples_per_feature -= res["num_samples"]
-            taken_or_estimated_samples = int(
-                res["taken_samples"] + torch.sum(required_samples_per_feature[required_samples_per_feature > 0])
-            )
+            res = method.explain_text(input_pair, pretokenized_text_data=pretokenized_example,
+                                      label=predicted_label, min_samples_per_feature=args.min_samples_per_feature,
+                                      custom_features=curr_features)
+            t2 = time()
 
-        logging.info(f"[{args.method}] {'taken' if compute_accurately else '(estimated) required'} "
-                     f"samples: {taken_or_estimated_samples}")
+            side_results["variances"] = res["var"].tolist()
+            side_results["num_samples"] = res["num_samples"].tolist()
+
+            if compute_accurately:
+                taken_or_estimated_samples = res['taken_samples']
+            else:
+                required_samples_per_feature = estimate_feature_samples(res["var"] * res["num_samples"],
+                                                                        alpha=(1 - args.confidence_interval),
+                                                                        max_abs_error=args.max_abs_error)
+                required_samples_per_feature -= res["num_samples"]
+                taken_or_estimated_samples = int(
+                    res["taken_samples"] + torch.sum(required_samples_per_feature[required_samples_per_feature > 0])
+                )
+
+            logging.info(f"[{args.method}] {'taken' if compute_accurately else '(estimated) required'} "
+                         f"samples: {taken_or_estimated_samples}")
+
         logging.info(f"[{args.method}] Time taken: {t2 - t1:.2f}s")
-
         sequence_tokens = res["input"]
 
         gen_samples = []
@@ -180,10 +191,11 @@ if __name__ == "__main__":
         method_data.add_example(sequence=sequence_tokens, label=predicted_label, probas=probas[0].tolist(),
                                 actual_label=actual_label, custom_features=curr_features,
                                 importances=res["importance"].tolist(),
-                                variances=res["var"].tolist(), num_samples=res["num_samples"].tolist(),
-                                samples=gen_samples, num_estimated_samples=res["taken_samples"], time_taken=(t2 - t1),
+                                num_estimated_samples=res["taken_samples"], time_taken=(t2 - t1),
+                                samples=gen_samples,
                                 model_scores=[[] if scores is None else scores
-                                              for scores in res["scores"]] if args.return_model_scores else [])
+                                              for scores in res["scores"]] if args.return_model_scores else [],
+                                **side_results)
 
         if (1 + idx_example) % args.save_every_n_examples == 0:
             logging.info(f"Saving data to {args.experiment_dir}")

explain_nlp/methods/lime.py
@@ -73,6 +73,7 @@ class LIMEExplainer:
             used_inds = list(range(num_features, num_features + num_additional))
 
         num_used = len(used_inds)
+        used_inds = torch.tensor(used_inds)
 
         eff_explanation_length = explanation_length if explanation_length is not None else num_used
         assert eff_explanation_length <= num_used
@@ -85,13 +86,13 @@ class LIMEExplainer:
         samples = instance.repeat((num_samples, 1))
 
         feature_indicators = torch.zeros((num_samples, num_features + num_additional), dtype=torch.bool)
-        feature_indicators[:, perturbable_inds] = True  # explained instance
+        feature_indicators[:, used_inds] = True  # explained instance
         removed_mask = torch.zeros_like(samples, dtype=torch.bool)
         for idx_sample in range(num_samples - 1):
             curr_removed_features = self.indexer(feature_groups, permuted_inds[idx_sample, :num_removed[idx_sample]])
 
             removed_mask[idx_sample + 1, curr_removed_features] = True
-            feature_indicators[idx_sample + 1, curr_removed_features] = False
+            feature_indicators[idx_sample + 1, used_inds[permuted_inds[idx_sample, :num_removed[idx_sample]]]] = False
 
         samples = self.generate_neighbourhood(samples, removal_mask=removed_mask, **modeling_kwargs)
 
@@ -146,6 +147,7 @@ class LIMEExplainer:
                            num_samples=num_samples, explanation_length=explanation_length,
                            custom_features=custom_features, **model_instance["aux_data"])
         res["input"] = self.model.convert_ids_to_tokens(model_instance["input_ids"])[0]
+        res["taken_samples"] = num_samples
 
         return res
 
@@ -242,21 +244,23 @@ class LIMEMaskedLMExplainer(LIMEExplainer):
                 cover_count[curr_group] += 1
                 free_features[0, curr_group] = False
 
-                mapped_group = list(map(lambda idx_feature: mapping[perturbable_position[idx_feature]],
-                                        curr_group))
+                mapped_group = []
+                for idx_feature in curr_group:
+                    mapped_group.extend(mapping[perturbable_position[idx_feature]])
+
                 feature_groups.append(curr_group)
                 generator_groups.append(mapped_group)
 
             for _, idx_feature in torch.nonzero(free_features, as_tuple=False):
                 free_features[0, idx_feature] = False
                 feature_groups.append([idx_feature])
-                generator_groups.append([mapping[perturbable_position[idx_feature]]])
+                generator_groups.append(mapping[perturbable_position[idx_feature]])
                 num_additional += 1
 
             used_inds = list(range(num_features, num_features + num_additional))
 
-        inds_group = dict(zip(used_inds, range(len(used_inds))))
         num_used = len(used_inds)
+        used_inds = torch.tensor(used_inds)
 
         eff_explanation_length = explanation_length if explanation_length is not None else num_used
         assert eff_explanation_length <= num_used
@@ -267,16 +271,15 @@ class LIMEMaskedLMExplainer(LIMEExplainer):
         num_removed = torch.randint(1, num_used + 1, size=(num_samples - 1,))
 
         feature_indicators = torch.zeros((num_samples, num_features + num_additional), dtype=torch.bool)
-        feature_indicators[:, perturbable_inds] = True  # explained instance
+        feature_indicators[:, used_inds] = True  # explained instance
 
         samples = instance_generator["input_ids"].repeat((num_samples, 1))
         removed_mask = torch.zeros_like(samples, dtype=torch.bool)
         for idx_sample in range(num_samples - 1):
-            curr_removed_model = self.indexer(feature_groups, permuted_inds[idx_sample, :num_removed[idx_sample]])
             curr_removed_generator = self.indexer(generator_groups, permuted_inds[idx_sample, :num_removed[idx_sample]])
 
             removed_mask[idx_sample + 1, curr_removed_generator] = True
-            feature_indicators[idx_sample + 1, curr_removed_model] = False
+            feature_indicators[idx_sample + 1, used_inds[permuted_inds[idx_sample, :num_removed[idx_sample]]]] = False
 
         samples = self.generate_neighbourhood(samples, removal_mask=removed_mask, **modeling_kwargs)
 