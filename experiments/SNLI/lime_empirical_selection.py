d0de4dc6fc308efd0eaec4e9a8f477052ed52166
Matej Klemen <matej.klemen1337@gmail.com>
2021-03-17 19:05:12 +0100

    Update LIME empirical selection experiment according to new argument structure

--- experiments/SNLI/lime_empirical_selection.py
+++ experiments/SNLI/lime_empirical_selection.py
@@ -1,24 +1,39 @@
 import json
+import logging
 import os
+import sys
+from time import time
 from typing import Union
 
-import torch
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+import torch
 
-from explain_nlp.experimental.arguments import parser
+from explain_nlp.experimental.arguments import lime_parser
 from explain_nlp.experimental.data import load_nli, LABEL_TO_IDX
 from explain_nlp.experimental.handle_explainer import load_explainer
 from explain_nlp.experimental.handle_generator import load_generator
 from explain_nlp.methods.lime import LIMEMaskedLMExplainer, LIMEExplainer
 from explain_nlp.modeling.modeling_transformers import InterpretableBertForSequenceClassification
 
-parser.add_argument("--num_repeats", type=int, default=10)
+lime_parser.add_argument("--num_repeats", type=int, default=10)
 
 if __name__ == "__main__":
-    args = parser.parse_args()
+    args = lime_parser.parse_args()
+    assert args.explanation_length is not None
     DEVICE = torch.device("cpu") if args.use_cpu else torch.device("cuda")
-    df_test = load_nli(args.test_path)
+
+    experiment_dir = f"{args.method}_{args.num_samples}samples_k{args.explanation_length}_{args.num_repeats}" \
+        if args.experiment_dir is None else args.experiment_dir
+    os.makedirs(experiment_dir)
+
+    # Set up logging to file and stdout
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    for curr_handler in [logging.StreamHandler(sys.stdout),
+                         logging.FileHandler(os.path.join(experiment_dir, "experiment.log"))]:
+        curr_handler.setFormatter(logging.Formatter("%(asctime)s [%(levelname)-5.5s]  %(message)s"))
+        logger.addHandler(curr_handler)
 
     model = InterpretableBertForSequenceClassification(tokenizer_name=args.model_dir,
                                                        model_name=args.model_dir,
@@ -28,39 +43,18 @@ if __name__ == "__main__":
                                                        device="cpu" if args.use_cpu else "cuda")
 
     generator, generator_description = load_generator(args, clm_labels=["<ENTAILMENT>", "<NEUTRAL>", "<CONTRADICTION>"])
-
-    assert args.method in ["lime", "lime_lm"]
-    method, method_type = load_explainer(method=args.method, model=model,
-                                         return_model_scores=args.return_model_scores,
-                                         return_generated_samples=args.return_generated_samples,
-                                         # Method-specific options below:
-                                         generator=generator, num_generated_samples=args.num_generated_samples,
-                                         kernel_width=args.kernel_width,
-                                         # unused, but currently required by the method
-                                         confidence_interval=None, max_abs_error=None)
-    method = method  # type: Union[LIMEExplainer, LIMEMaskedLMExplainer]
-
-    NUM_SAMPLES = args.min_samples_per_feature
-    NUM_REPEATS = args.num_repeats
-    assert args.explanation_length is not None
-    EXPLANATION_LENGTH = args.explanation_length
-
-    experiment_dir = f"{args.method}_{NUM_SAMPLES}samples_k{EXPLANATION_LENGTH}_{NUM_REPEATS}" \
-        if args.experiment_dir is None else args.experiment_dir
-    os.makedirs(experiment_dir)
+    args_dict = vars(args)
+    args_dict["method_class"] = "lime"
+    method, method_type = load_explainer(model=model, generator=generator,
+                                         **args_dict)  # type: Union[LIMEExplainer, LIMEMaskedLMExplainer]
 
     with open(os.path.join(experiment_dir, "experiment_config.json"), "w") as f_config:
-        json.dump({
-            "test_path": args.test_path,
-            "method": args.method,
-            "num_samples": NUM_SAMPLES,
-            "explanation_length": EXPLANATION_LENGTH,
-            "num_repeats": NUM_REPEATS
-        }, fp=f_config, indent=4)
+        json.dump(vars(args), fp=f_config, indent=4)
 
+    df_test = load_nli(args.test_path)
     selected_features_per_example = []
     for idx_example, input_pair in enumerate(df_test[["sentence1", "sentence2"]].values.tolist()):
-        print(f"Processing {input_pair}")
+        logging.info(f"Processing {input_pair}")
         encoded_example = model.to_internal(text_data=[input_pair])
         probas = model.score(input_ids=encoded_example["input_ids"].to(DEVICE),
                              **{k: v.to(DEVICE) for k, v in encoded_example["aux_data"].items()})
@@ -70,12 +64,15 @@ if __name__ == "__main__":
         num_total_features = None
 
         selected_features = []
-        for idx_rep in range(NUM_REPEATS):
+        for idx_rep in range(args.num_repeats):
+            ts = time()
             res = method.explain_text(text_data=input_pair, label=predicted_label,
-                                      num_samples=NUM_SAMPLES, explanation_length=EXPLANATION_LENGTH)
+                                      num_samples=args.num_samples, explanation_length=args.explanation_length)
             curr_selected = torch.flatten(torch.nonzero(res["importance"])).tolist()
             selected_features.append(curr_selected)
             num_total_features = int(res["importance"].shape[0])
+            te = time()
+            logging.info(f"\t[Rep#{idx_rep}] Time taken: {te - ts: .4f}s")
 
         counter = np.zeros(num_total_features)
         for curr_selected in selected_features:
@@ -83,12 +80,12 @@ if __name__ == "__main__":
 
         selected_features_per_example.append(int(np.sum(counter > 0)))
 
-        plt.title(f"Ex.#{idx_example}: Empirical selection frequency (/{NUM_REPEATS} reps) "
-                  f"of K={EXPLANATION_LENGTH}-sparse LIME")
+        plt.title(f"Ex.#{idx_example}: Empirical selection frequency (/{args.num_repeats} reps) "
+                  f"of K={args.explanation_length}-sparse LIME")
         plt.bar(np.arange(num_total_features), counter)
 
-        plt.ylim([0, NUM_REPEATS + 1])
-        plt.yticks(np.arange(0, NUM_REPEATS + 1, 10))
+        plt.ylim([0, args.num_repeats + 1])
+        plt.yticks(np.arange(0, args.num_repeats + 1, 10))
 
         plt.xlabel("Feature")
         plt.xticks(np.arange(num_total_features))
@@ -104,6 +101,6 @@ if __name__ == "__main__":
             "sd_selected_features": np.std(selected_features_per_example)
         }, fp=f, indent=4)
 
-    print(f"Selected features per example: "
-          f"mean={np.mean(selected_features_per_example)},"
-          f"sd={np.std(selected_features_per_example)}")
+    logging.info(f"[RESULTS] Selected features per example: "
+                 f"mean={np.mean(selected_features_per_example)}, "
+                 f"sd={np.std(selected_features_per_example)}")
