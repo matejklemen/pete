73d222d007fd0faed0267934430985d74dea90ca
Matej Klemen <matej.klemen1337@gmail.com>
2021-03-19 18:53:05 +0100

    Set return_metrics back to True by default

--- explain_nlp/methods/lime.py
+++ explain_nlp/methods/lime.py
@@ -20,7 +20,7 @@ def exponential_kernel(dists: torch.Tensor, kernel_width: float):
 class LIMEExplainer:
     def __init__(self, model: InterpretableModel, kernel_width=25.0,
                  return_samples: Optional[bool] = False, return_scores: Optional[bool] = False,
-                 return_metrics: Optional[bool] = False):
+                 return_metrics: Optional[bool] = True):
         self.model = model
         self.kernel_width = kernel_width
 
@@ -170,7 +170,7 @@ class LIMEExplainer:
 class LIMEMaskedLMExplainer(LIMEExplainer):
     def __init__(self, model: InterpretableModel, generator: SampleGenerator, kernel_width=25.0,
                  return_samples: Optional[bool] = False, return_scores: Optional[bool] = False,
-                 return_metrics: Optional[bool] = False, is_aligned_vocabulary: Optional[bool] = False):
+                 return_metrics: Optional[bool] = True, is_aligned_vocabulary: Optional[bool] = False):
         super().__init__(model=model, kernel_width=kernel_width,
                          return_samples=return_samples, return_scores=return_scores, return_metrics=return_metrics)
         self.generator = generator
