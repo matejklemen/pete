6045e233f2bf99c7f8a20dc43166dd7210e9fbf3
Matej Klemen <matej.klemen1337@gmail.com>
2021-03-26 15:34:02 +0100

    Add option to use bigger units in LIME experiment

--- experiments/SNLI/lime_empirical_selection.py
+++ experiments/SNLI/lime_empirical_selection.py
@@ -7,11 +7,13 @@ from typing import Union
 
 import matplotlib.pyplot as plt
 import numpy as np
+import stanza
 import torch
 
 from explain_nlp.experimental.arguments import lime_parser
 from explain_nlp.experimental.data import load_nli, LABEL_TO_IDX, IDX_TO_LABEL
 from explain_nlp.experimental.handle_explainer import load_explainer
+from explain_nlp.experimental.handle_features import handle_features
 from explain_nlp.experimental.handle_generator import load_generator
 from explain_nlp.methods.lime import LIMEMaskedLMExplainer, LIMEExplainer
 from explain_nlp.modeling.modeling_transformers import InterpretableBertForSequenceClassification
@@ -77,14 +79,46 @@ if __name__ == "__main__":
         logging.info(f"Loaded existing experiment data - continuing from example#{start_from}")
 
     df_test = load_nli(args.test_path)
+
+    nlp, pretokenized_test_data = None, None
+    if args.custom_features is not None:
+        nlp = stanza.Pipeline(lang="en", processors="tokenize", use_gpu=not args.use_cpu, tokenize_no_ssplit=True)
+        pretokenized_test_data = []
+        for idx_subset in range((df_test.shape[0] + 1024 - 1) // 1024):
+            s, e = idx_subset * 1024, (1 + idx_subset) * 1024
+            for s0, s1 in zip(nlp("\n\n".join(df_test["sentence1"].iloc[s: e].values)).sentences,
+                              nlp("\n\n".join(df_test["sentence2"].iloc[s: e].values)).sentences):
+                pretokenized_test_data.append((
+                    [token.words[0].text for token in s0.tokens],
+                    [token.words[0].text for token in s1.tokens]
+                ))
+
+        # Reload pipeline if depparse features are used (not done from the start as this would slow down tokenization)
+        if args.custom_features.startswith("depparse"):
+            nlp = stanza.Pipeline(lang="en", processors="tokenize,lemma,pos,depparse")
+
     for idx_example, input_pair in enumerate(df_test[["sentence1", "sentence2"]].values[start_from:].tolist(),
                                              start=start_from):
         logging.info(f"#{idx_example}. Processing {input_pair}")
-        encoded_example = model.to_internal(text_data=[input_pair])
+
+        if args.custom_features is not None:
+            encoded_example = model.to_internal(pretokenized_text_data=[pretokenized_test_data[idx_example]])
+        else:
+            encoded_example = model.to_internal(text_data=[input_pair])
+
         probas = model.score(input_ids=encoded_example["input_ids"].to(DEVICE),
                              **{k: v.to(DEVICE) for k, v in encoded_example["aux_data"].items()})
         predicted_label = int(torch.argmax(probas))
         actual_label = int(LABEL_TO_IDX["snli"][df_test.iloc[idx_example]["gold_label"]])
+
+        pretokenized_example, curr_features = None, None
+        if args.custom_features is not None:
+            # Obtain word IDs for subwords in all cases as the custom features are usually obtained from words
+            word_ids = encoded_example["aux_data"]["alignment_ids"][0].tolist()
+            curr_features = handle_features(args.custom_features, word_ids=word_ids, pipe=nlp,
+                                            raw_example=(df_test.iloc[idx_example]["sentence1"],
+                                                         df_test.iloc[idx_example]["sentence2"]))
+            pretokenized_example = pretokenized_test_data[idx_example]
 
         num_total_features = None
         reps_data = []
@@ -95,6 +129,7 @@ if __name__ == "__main__":
         for idx_rep in range(args.num_repeats):
             ts = time()
             res = method.explain_text(text_data=input_pair, label=predicted_label,
+                                      pretokenized_text_data=pretokenized_example, custom_features=curr_features,
                                       num_samples=args.num_samples, explanation_length=args.explanation_length)
             te = time()
             logging.info(f"\n[Rep#{idx_rep}] (time taken={te - ts: .2f}s)")
@@ -123,10 +158,12 @@ if __name__ == "__main__":
         highlight_plot([best_res["input"]], importances=[best_res["importance"].tolist()],
                        pred_labels=[IDX_TO_LABEL["snli"][predicted_label]],
                        actual_labels=[IDX_TO_LABEL["snli"][actual_label]],
+                       custom_features=[curr_features] if args.custom_features is not None else None,
                        path=os.path.join(experiment_dir, "explanations", f"ex{idx_example}_best_explanation.html"))
         highlight_plot([worst_res["input"]], importances=[worst_res["importance"].tolist()],
                        pred_labels=[IDX_TO_LABEL["snli"][predicted_label]],
                        actual_labels=[IDX_TO_LABEL["snli"][actual_label]],
+                       custom_features=[curr_features] if args.custom_features is not None else None,
                        path=os.path.join(experiment_dir, "explanations", f"ex{idx_example}_worst_explanation.html"))
 
         counter = np.zeros(num_total_features)
