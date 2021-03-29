4ecb796b8b3555a3fbccef906399fac9cd5c101c
Matej Klemen <matej.klemen1337@gmail.com>
2021-03-29 22:40:00 +0200

    Add IoU, SNR, HPD metrics

--- explain_nlp/utils/metrics.py
+++ explain_nlp/utils/metrics.py
@@ -1,7 +1,11 @@
+import warnings
+from typing import Union
+
 import numpy as np
+from sklearn.metrics import jaccard_score, precision_score
 
 
-def fidelity(model_scores: np.ndarray, surrogate_scores: np.ndarray):
+def fidelity(model_scores: Union[float, np.ndarray], surrogate_scores: Union[float, np.ndarray]):
     """ Measures how well a surrogate model approximates the original model.
     Assuming the scores are probabilities, fidelity ranges from 0.5 (worst) to 1.0 (best).
 
@@ -14,5 +18,76 @@ def fidelity(model_scores: np.ndarray, surrogate_scores: np.ndarray):
     return 1.0 / (np.abs(model_scores - surrogate_scores) + 1.0)
 
 
-if __name__ == "__main__":
-    print(fidelity(np.array([1.0, 0.5, 0.2, 0.4]), np.array([0.0, 0.5, 0.4, 0.2])))
+def iou_score(y_true: np.ndarray, y_pred: np.ndarray):
+    """ Compute intersection over union between ground truth sentence and predicted sentences, where each is a
+    binary array.
+
+    Args:
+        y_true:
+            One-hot array, where the nonzero element corresponds to the correct sentence
+        y_pred:
+            Binary array, whose elements are 1 if that sentence is predicted to be important
+    """
+    assert np.any(y_true)
+
+    return jaccard_score(y_true, y_pred)
+
+
+def hpd_score(sentence_ordering: np.ndarray, gt: np.ndarray):
+    """ Compute highest precision for detection, which is the precision when all sentences with a score above
+    (or equal to) ground truth sentence are taken as predictions by the method.
+
+    Intuitively, this measures how many additional (false positive) sentences get predicted in addition to the correct
+    sentence.
+
+    Args:
+        sentence_ordering:
+            Array that specifies the ordering of sentence scores as provided by a method
+        gt:
+            One-hot array, where the nonzero element corresponds to the correct sentence
+    """
+    assert len(sentence_ordering) == len(gt)
+    gt_bool = gt.astype(np.bool)
+
+    # sentence_ranks[i]: in which place is the element `i` in `sentence_ordering`
+    # e.g. sentence_ranks[3]: which rank does sentence 3 take?
+    sentence_ranks = np.empty_like(sentence_ordering)
+    sentence_ranks[sentence_ordering] = np.arange(len(sentence_ordering))
+
+    gt_rank = sentence_ranks[gt_bool]
+    if len(gt_rank) != 1:
+        raise ValueError(f"Exactly one ground truth sentence is allowed "
+                         f"(`gt` contains {len(gt_rank)} nonzero elements)")
+
+    pred = np.zeros_like(gt_bool)
+    pred[sentence_ordering[:int(gt_rank) + 1]] = True
+
+    return precision_score(y_true=gt_bool, y_pred=pred)
+
+
+def snr_score(sentence_scores: np.ndarray, gt: np.ndarray):
+    """ Compute signal to noise ratio for a single sentence.
+
+    Args:
+        sentence_scores:
+            Scores of sentences, assigned by a method
+        gt:
+            Binary array, in which 1 denotes a correct sentence and 0 a wrong sentence
+    """
+    gt_bool = gt.astype(np.bool)
+
+    correct_score = sentence_scores[gt_bool]
+    incorrect_scores = sentence_scores[np.logical_not(gt_bool)]
+
+    if len(correct_score) != 1:
+        raise ValueError(f"Exactly one ground truth sentence is allowed "
+                         f"(`gt` contains {len(correct_score)} nonzero elements)")
+
+    numerator = np.square(float(correct_score) - np.mean(incorrect_scores))
+    denominator = np.square(np.std(incorrect_scores))
+    if denominator == 0.0:
+        warnings.warn("Encountered zero deviation for incorrect sentence scores. "
+                      "Setting denominator (sq. deviation) to 1.0 to avoid division by zero")
+        denominator = 1.0
+
+    return numerator / denominator
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,6 +5,7 @@ import numpy as np
 
 from explain_nlp.methods.utils import estimate_feature_samples, sample_permutations, \
     incremental_mean, incremental_var
+from explain_nlp.utils.metrics import iou_score, fidelity, snr_score, hpd_score
 
 
 class TestUtils(unittest.TestCase):
@@ -57,3 +58,82 @@ class TestUtils(unittest.TestCase):
 
             self.assertAlmostEqual(var_way1, var_way2, places=7)
             acc_var = var_way2
+
+    def test_fidelity(self):
+        # completely different prediction
+        self.assertAlmostEqual(fidelity(1.0, 0.0), 0.5)
+        # correct prediction
+        self.assertAlmostEqual(fidelity(0.5, 0.5), 1.0)
+        # symmetry
+        self.assertAlmostEqual(fidelity(0.2, 0.4), 1.0/1.2)
+        self.assertAlmostEqual(fidelity(0.4, 0.2), 1.0/1.2)
+
+        np.testing.assert_allclose(
+            fidelity(np.array([1.0, 0.5, 0.2, 0.4]), np.array([0.0, 0.5, 0.4, 0.2])),
+            np.array([0.5, 1.0, 1.0/1.2, 1.0/1.2])
+        )
+
+    def test_iou(self):
+        gt_sents_general = np.array([1, 0, 1, 0, 0])
+        gt_sents_all = np.array([1, 1, 1, 1, 1])
+        gt_sents_one = np.array([0, 0, 1, 0, 0])
+        gt_sents_none = np.array([0, 0, 0, 0, 0])
+
+        pred_sents = np.array([0, 0, 1, 0, 0])
+        self.assertAlmostEqual(iou_score(y_true=gt_sents_general, y_pred=pred_sents), 1/2)
+        self.assertAlmostEqual(iou_score(y_true=gt_sents_all, y_pred=pred_sents), 1/5)
+        self.assertAlmostEqual(iou_score(y_true=gt_sents_one, y_pred=pred_sents), 1.0)
+        with self.assertRaises(AssertionError):
+            iou_score(y_true=gt_sents_none, y_pred=pred_sents)
+
+        pred_sents = np.array([1, 1, 1, 1, 1])
+        self.assertAlmostEqual(iou_score(y_true=gt_sents_general, y_pred=pred_sents), 2/5)
+        self.assertAlmostEqual(iou_score(y_true=gt_sents_all, y_pred=pred_sents), 1.0)
+        self.assertAlmostEqual(iou_score(y_true=gt_sents_one, y_pred=pred_sents), 1/5)
+        with self.assertRaises(AssertionError):
+            iou_score(y_true=gt_sents_none, y_pred=pred_sents)
+
+        pred_sents = np.array([0, 0, 0, 0, 0])
+        self.assertAlmostEqual(iou_score(y_true=gt_sents_general, y_pred=pred_sents), 0.0)
+
+    def test_hpd(self):
+        sent_scores = np.array([0.1, 0.2, 0.05, 0.3, -0.05])
+        sent_order = np.argsort(-sent_scores)  # descending
+
+        # Best case: sentence 3 is correct and its score is highest, meaning no false positives
+        np.testing.assert_allclose(hpd_score(sent_order, gt=np.eye(1, len(sent_scores), k=3, dtype=np.int32)[0]),
+                                   1.0)
+        # Worst case: sentence 4 is correct, but its score is lowest, meaning we predict 4 FP an 1 TP
+        np.testing.assert_allclose(hpd_score(sent_order, gt=np.eye(1, len(sent_scores), k=4, dtype=np.int32)[0]),
+                                   1/5)
+
+        # General case (somewhere in between)
+        np.testing.assert_allclose(hpd_score(sent_order, gt=np.eye(1, len(sent_scores), k=1, dtype=np.int32)[0]),
+                                   1/2)
+
+        # Invalid inputs:
+        # 1) no ground truth sentence
+        with self.assertRaises(ValueError):
+            hpd_score(sent_order, gt=np.zeros(len(sent_scores)))
+
+        # 2) more than one ground truth sentence
+        with self.assertRaises(ValueError):
+            hpd_score(sent_order, gt=np.array([1, 1, 0, 0, 0]))
+
+    def test_snr(self):
+        uniform_scores = np.array([0.2, 0.2, 0.2, 0.2, 0.2])
+        uniform_noise_scores = np.array([0.2, 0.4, 0.2, 0.2, 0.2])
+        general_scores = np.array([0.0, 1.0, -2.0, 0.1, 0.1])
+
+        # sentence 1 is correct
+        gt_vector = np.eye(1, 5, k=1, dtype=np.int32)[0]
+
+        np.testing.assert_allclose(snr_score(general_scores, gt=gt_vector), 2.6199, atol=10e-4)
+
+        # ill-defined cases: deviation of noise scores is zero, resulting in division by zero
+        # convention: set denominator to one in that case
+        with self.assertWarns(UserWarning):
+            np.testing.assert_allclose(snr_score(uniform_scores, gt=gt_vector), 0.0)
+
+        with self.assertWarns(UserWarning):
+            np.testing.assert_allclose(snr_score(uniform_noise_scores, gt=gt_vector), 0.04)
