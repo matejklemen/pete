175a84915279d8b98c78baf43d7176192a01e71b
Matej Klemen <matej.klemen1337@gmail.com>
2021-06-20 19:17:45 +0200

    Add support for IME in distribution detection

--- experiments/distribution_detection/embedding.py
+++ experiments/distribution_detection/embedding.py
@@ -8,9 +8,11 @@ import torch
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
-from explain_nlp.experimental.data import TransformerSeqPairDataset, LABEL_TO_IDX
+from explain_nlp.experimental.data import TransformerSeqPairDataset, LABEL_TO_IDX, load_nli
 from explain_nlp.experimental.handle_explainer import load_explainer
 from explain_nlp.experimental.handle_generator import load_generator
+from explain_nlp.generation.decoding import filter_factory
+from explain_nlp.methods.ime_lm import create_uniform_weights
 from explain_nlp.modeling.modeling_transformers import InterpretableBertForSequenceClassification
 
 control_parser = subparsers.add_parser("control", parents=[general_parser])
@@ -87,10 +89,27 @@ if __name__ == "__main__":
         generator, gen_description = load_generator(args,
                                                     clm_labels=["<ENTAILMENT>", "<NEUTRAL>", "<CONTRADICTION>"])
 
+        if args.method_class == "ime" and generator is not None:
+            generator.filters = [filter_factory("unique")] + generator.filters
+
+        used_sample_data = None
+        data_weights = None
+        if args.method in {"ime", "ime_hybrid"}:
+            df_train = load_nli(args.train_path).sample(frac=1.0).reset_index(drop=True)  # TODO: generalize
+            used_tokenizer = model.tokenizer if args.method == "ime" else generator.tokenizer
+            # TODO: generalize
+            train_set = TransformerSeqPairDataset.build(df_train["sentence1"].tolist(), df_train["sentence2"].tolist(),
+                                                        labels=df_train["gold_label"].apply(
+                                                            lambda label_str: LABEL_TO_IDX["snli"][label_str]).tolist(),
+                                                        tokenizer=used_tokenizer, max_seq_len=args.model_max_seq_len)
+            data_weights = create_uniform_weights(train_set.input_ids, train_set.special_tokens_masks)
+            used_sample_data = train_set.input_ids
+
         #   TODO: load explainer (all args..., set them to reasonable defaults)
         method, method_type = load_explainer(
             method_class=args.method_class, method=args.method, model=model, generator=generator,
-            return_generated_samples=True, kernel_width=1.0, shared_vocabulary=True
+            used_sample_data=used_sample_data, data_weights=data_weights, experiment_type="required_samples",
+            return_generated_samples=True, kernel_width=1.0, shared_vocabulary=True, num_generated_samples=10
         )
 
         # These hold encoded perturbed samples, which will be embedded with model and used in distribution detection
@@ -110,12 +129,24 @@ if __name__ == "__main__":
             probas = model.score(input_ids=encoded_example["input_ids"],
                                  **{k: encoded_example["aux_data"][k] for k in ["token_type_ids", "attention_mask"]})
             predicted_label = int(torch.argmax(probas))
-            # print(f"[pred={predicted_label}] {tup_input_pair}")
 
-            res = method.explain_text(tup_input_pair, label=predicted_label,
-                                      num_samples=5, explanation_length=args.explanation_length)
-            samples = res["samples"]
-            idx_selected = np.random.randint(1, len(samples))  # sample[0] is by convention the original sample in LIME
+            if args.method_class == "lime":
+                res = method.explain_text(tup_input_pair, label=predicted_label,
+                                          num_samples=5, explanation_length=args.explanation_length)
+                samples = res["samples"]
+            else:
+                # In IME, select random sample from a random explained feature
+                perturbable_indices = \
+                    torch.arange(encoded_example["input_ids"].shape[1])[encoded_example["perturbable_mask"][0]]
+                selected_feature = perturbable_indices[torch.randint(perturbable_indices.shape[0], ())].item()
+
+                res = method.explain_text(tup_input_pair, label=predicted_label,
+                                          min_samples_per_feature=2)
+                samples = res["samples"][selected_feature]
+
+            # sample[0] is by convention the original sample in LIME
+            idx_selected = np.random.randint(1 if args.method_class == "lime" else 0,
+                                             len(samples))
             input_ids.append(samples[idx_selected])
 
             # First example: initialize lists for additional data
