9ad1dcdf00a2ab6c4b7af3681f12d4814ff20a6d
Matej Klemen <matej.klemen1337@gmail.com>
2021-06-28 12:06:36 +0200

    Generalize distribution detection script for multiple datasets

--- experiments/distribution_detection/embedding.py
+++ experiments/distribution_detection/embedding.py
@@ -1,23 +1,31 @@
 import json
+import logging
 import os
+import sys
+from typing import Tuple
+
 import numpy as np
 import pandas as pd
-from explain_nlp.experimental.arguments import methods_parser, subparsers, general_parser
-
+import stanza
 import torch
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
-from explain_nlp.experimental.data import TransformerSeqPairDataset, LABEL_TO_IDX, load_nli
+from explain_nlp.experimental.arguments import methods_parser, subparsers, general_parser
+from explain_nlp.experimental.data import TransformerSeqDataset, TransformerSeqPairDataset, load_dataset, \
+    IDX_TO_LABEL, PRESET_COLNAMES
 from explain_nlp.experimental.handle_explainer import load_explainer
 from explain_nlp.experimental.handle_generator import load_generator
 from explain_nlp.generation.decoding import filter_factory
+from explain_nlp.methods.features import extract_groups
 from explain_nlp.methods.ime_lm import create_uniform_weights
 from explain_nlp.modeling.modeling_transformers import InterpretableBertForSequenceClassification
 
 control_parser = subparsers.add_parser("control", parents=[general_parser])
 control_parser.add_argument("--method", choices=["control"],
                             default="control", help="Fixed argument that is here just for consistency")
+
+STANZA_BATCH_SIZE = 1024
 
 
 @torch.no_grad()
@@ -27,6 +35,39 @@ def bert_embeddings(model: InterpretableBertForSequenceClassification, input_ids
                               **{attr: modeling_kwargs[attr].to(model.device)
                                  for attr in ["token_type_ids", "attention_mask"]})
     return output["pooler_output"]  # [num_examples, hidden_size]
+
+
+def stanza_tokenize(stanza_pipeline, data, dataset_name):
+    # _tokenize() tokenizes a batch of examples with Stanza pipeline
+    # _unwrap() extracts the tokens into a list or pair of lists (depending on input)
+    if dataset_name in ["snli", "mnli", "xnli", "qqp"]:
+        col1, col2 = PRESET_COLNAMES[dataset_name]
+
+        def _tokenize(start_batch, end_batch):
+            return (stanza_pipeline("\n\n".join(data[col1].iloc[start_batch: end_batch].values)).sentences,
+                    stanza_pipeline("\n\n".join(data[col2].iloc[start_batch: end_batch].values)).sentences)
+
+        def _unwrap(tokenized_pair: Tuple):
+            return (
+                [token.words[0].text for token in tokenized_pair[0].tokens],
+                [token.words[0].text for token in tokenized_pair[1].tokens]
+            )
+    else:
+        col = PRESET_COLNAMES[dataset_name][0]
+
+        def _tokenize(start_batch, end_batch):
+            return (stanza_pipeline("\n\n".join(data[col].iloc[start_batch: end_batch].values)).sentences,)
+
+        def _unwrap(tokenized_seq):
+            return [token.words[0].text for token in tokenized_seq.tokens]
+
+    pretokenized_test_data = []
+    for idx_subset in range((data.shape[0] + STANZA_BATCH_SIZE - 1) // STANZA_BATCH_SIZE):
+        s, e = idx_subset * STANZA_BATCH_SIZE, (1 + idx_subset) * STANZA_BATCH_SIZE
+        for tokenized_data in zip(*_tokenize(s, e)):
+            pretokenized_test_data.append(_unwrap(tokenized_data))
+
+    return pretokenized_test_data
 
 
 if __name__ == "__main__":
@@ -39,12 +80,13 @@ if __name__ == "__main__":
         np.random.seed(args.random_seed)
         torch.manual_seed(args.random_seed)
 
+    with open(os.path.join(args.experiment_dir, "config.json"), "r", encoding="utf-8") as f:
+        experiment_config = json.load(f)
+
     # Load sample data
     sample_path = os.path.join(args.experiment_dir, "sample.csv")
     assert os.path.exists(sample_path)
     df_sample = pd.read_csv(sample_path)
-
-    use_control = (args.method_class == "control")
 
     mini_experiment_path = os.path.join(args.experiment_dir, args.method)
     if not os.path.exists(mini_experiment_path):
@@ -53,22 +95,33 @@ if __name__ == "__main__":
     with open(os.path.join(mini_experiment_path, "embedding_config.json"), "w", encoding="utf-8") as f:
         json.dump(vars(args), fp=f, indent=4)
 
-    # Load interpreted model
-    # TODO: generalize
+    # Set up logging to file and stdout
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    for curr_handler in [logging.StreamHandler(sys.stdout),
+                         logging.FileHandler(os.path.join(mini_experiment_path, "experiment.log"))]:
+        curr_handler.setFormatter(logging.Formatter("%(asctime)s [%(levelname)-5.5s]  %(message)s"))
+        logger.addHandler(curr_handler)
+
+    # Load interpreted model (TODO: generalize if using other model classes)
     model = InterpretableBertForSequenceClassification(
         model_name=args.model_dir, tokenizer_name=args.model_dir,
         batch_size=args.model_batch_size, max_seq_len=args.model_max_seq_len,
         device="cpu" if args.use_cpu else "cuda"
     )
-    model.model.eval()
 
-    # TODO: generalize
-    sample_dataset = TransformerSeqPairDataset.build(
-        first=df_sample["sentence1"].tolist(), second=df_sample["sentence2"].tolist(),
-        labels=df_sample["gold_label"].apply(lambda label_str: LABEL_TO_IDX["snli"][label_str]).tolist(),
-        tokenizer=model.tokenizer, max_seq_len=args.model_max_seq_len
-    )
+    logging.info(f"Building sample dataset (preset_config={experiment_config['dataset']}) with "
+                 f"{df_sample.shape[0]} examples")
+    try:
+        sample_dataset = TransformerSeqDataset.build_dataset(experiment_config["dataset"], df_sample,
+                                                             tokenizer=model.tokenizer,
+                                                             max_seq_len=args.model_max_seq_len)
+    except NotImplementedError:
+        sample_dataset = TransformerSeqPairDataset.build_dataset(experiment_config["dataset"], df_sample,
+                                                                 tokenizer=model.tokenizer,
+                                                                 max_seq_len=args.model_max_seq_len)
 
+    use_control = (args.method_class == "control")
     other_embeddings = []
     # Control group is a non-overlapping group of examples from the same set as the main sample:
     # The idea is to see how well a complex model can distinguish examples from the same (empirical) distribution
@@ -76,82 +129,135 @@ if __name__ == "__main__":
         control_path = os.path.join(args.experiment_dir, "control.csv")
         df_control = pd.read_csv(control_path)
 
-        # TODO: generalize
-        control_dataset = TransformerSeqPairDataset.build(
-            first=df_control["sentence1"].tolist(), second=df_control["sentence2"].tolist(),
-            labels=df_control["gold_label"].apply(lambda label_str: LABEL_TO_IDX["snli"][label_str]).tolist(),
-            tokenizer=model.tokenizer, max_seq_len=args.model_max_seq_len
-        )
+        logging.info(f"Building control dataset (preset_config={experiment_config['dataset']}) with "
+                     f"{df_control.shape[0]} examples")
+        try:
+            control_dataset = TransformerSeqDataset.build_dataset(experiment_config["dataset"], df_control,
+                                                                  tokenizer=model.tokenizer,
+                                                                  max_seq_len=args.model_max_seq_len)
+        except NotImplementedError:
+            control_dataset = TransformerSeqPairDataset.build_dataset(experiment_config["dataset"], df_control,
+                                                                      tokenizer=model.tokenizer,
+                                                                      max_seq_len=args.model_max_seq_len)
 
+        logging.info("Embedding control dataset with interpreted model")
         for curr_batch in tqdm(DataLoader(control_dataset, batch_size=args.model_batch_size)):
             other_embeddings.append(bert_embeddings(model, **curr_batch).cpu())
     else:
-        generator, gen_description = load_generator(args,
-                                                    clm_labels=["<ENTAILMENT>", "<NEUTRAL>", "<CONTRADICTION>"])
+        # Assumption: all used models use control labels, formatted as "<LABEL_NAME>"
+        possible_labels = IDX_TO_LABEL[experiment_config["dataset"]]
+        clm_labels = [f"<{label_name.upper()}>"
+                      for _, label_name in sorted(possible_labels.items(), key=lambda tup: tup[0])]
+        generator, gen_description = load_generator(args, clm_labels=clm_labels)
 
+        nlp = stanza.Pipeline(lang="en", processors="tokenize", use_gpu=not args.use_cpu, tokenize_no_ssplit=True)
+        pretokenized_test_data = [None for _ in range(df_sample.shape[0])]
+        if args.custom_features is not None:
+            logging.info(f"Tokenizing explained instances with Stanza")
+            pretokenized_test_data = stanza_tokenize(nlp, df_sample, experiment_config["dataset"])
+
+            assert args.custom_features in ["words", "dependency_parsing"], f"In distribution detection experiment, " \
+                                                                            f"'{args.custom_features}' is unsupported"
+
+            if args.custom_features == "dependency_parsing":
+                nlp = stanza.Pipeline(lang="en", processors="tokenize,lemma,pos,depparse")
+
+        # Special restriction: ensure generated tokens are unique - otherwise, it is theoretically possible to achieve
+        #  perfect score (~50% discrimination accuracy) by just copying the input
         if args.method_class == "ime" and generator is not None:
+            logging.info("Adding unique filter to generator used in IME method")
             generator.filters = [filter_factory("unique")] + generator.filters
 
-        used_sample_data = None
-        data_weights = None
+        used_sample_data, data_weights = None, None
         if args.method in {"ime", "ime_hybrid"}:
-            df_train = load_nli(args.train_path).sample(frac=1.0).reset_index(drop=True)  # TODO: generalize
+            logging.info(f"Loading sampling data (dataset={experiment_config['dataset']})")
+            df_train = load_dataset(experiment_config["dataset"], file_path=args.train_path)
+            df_train = df_train.sample(frac=1.0).reset_index(drop=True)
+
+            logging.info(f"Building sampling dataset (preset_config={experiment_config['dataset']}) with "
+                         f"{df_train.shape[0]} examples")
             used_tokenizer = model.tokenizer if args.method == "ime" else generator.tokenizer
-            # TODO: generalize
-            train_set = TransformerSeqPairDataset.build(df_train["sentence1"].tolist(), df_train["sentence2"].tolist(),
-                                                        labels=df_train["gold_label"].apply(
-                                                            lambda label_str: LABEL_TO_IDX["snli"][label_str]).tolist(),
-                                                        tokenizer=used_tokenizer, max_seq_len=args.model_max_seq_len)
+            used_length = args.model_max_seq_len if args.method == "ime" else args.generator_max_seq_len
+            try:
+                train_set = TransformerSeqDataset.build_dataset(experiment_config["dataset"], df_train,
+                                                                tokenizer=used_tokenizer, max_seq_len=used_length)
+            except NotImplementedError:
+                train_set = TransformerSeqPairDataset.build_dataset(experiment_config["dataset"], df_train,
+                                                                    tokenizer=used_tokenizer, max_seq_len=used_length)
+
             data_weights = create_uniform_weights(train_set.input_ids, train_set.special_tokens_masks)
             used_sample_data = train_set.input_ids
 
-        #   TODO: load explainer (all args..., set them to reasonable defaults)
+        logging.info("Instantiating explanation method")
         method, method_type = load_explainer(
-            method_class=args.method_class, method=args.method, model=model, generator=generator,
-            used_sample_data=used_sample_data, data_weights=data_weights, experiment_type="required_samples",
-            return_generated_samples=True, kernel_width=1.0, shared_vocabulary=True, num_generated_samples=10
+            method_class=args.method_class, method=args.method,
+            model=model, generator=generator,
+            used_sample_data=used_sample_data, data_weights=data_weights,
+            experiment_type="required_samples", return_generated_samples=True,
+            kernel_width=1.0, shared_vocabulary=True, num_generated_samples=10
         )
 
         # These hold encoded perturbed samples, which will be embedded with model and used in distribution detection
         input_ids, modeling_data = [], {}
 
+        raw_examples = df_sample[PRESET_COLNAMES[experiment_config["dataset"]]].values
+        raw_examples = list(map(lambda row: row[0] if len(row) == 1 else tuple(row), raw_examples))
+
         # For each instance, sample 1 perturbation
-        for idx_ex in tqdm(range(df_sample.shape[0]), total=df_sample.shape[0]):
-            example = df_sample.iloc[idx_ex]
-            tup_input_pair = (example["sentence1"], example["sentence2"])
+        logging.info("Sampling perturbations")
+        for raw_input, pretok_input in tqdm(zip(raw_examples, pretokenized_test_data), total=len(raw_examples)):
+            is_pretok = pretok_input is not None
 
-            # TODO: support for pretokenized
+            # Obtain the predicted label, which the instance will be explained for
             encoded_example = model.to_internal(
-                text_data=[tup_input_pair],
-                is_split_into_units=False
+                text_data=[pretok_input if is_pretok else raw_input],
+                is_split_into_units=is_pretok
             )
-
             probas = model.score(input_ids=encoded_example["input_ids"],
                                  **{k: encoded_example["aux_data"][k] for k in ["token_type_ids", "attention_mask"]})
             predicted_label = int(torch.argmax(probas))
 
+            feature_groups = None
+            if args.custom_features is not None:
+                word_ids = encoded_example["aux_data"]["alignment_ids"][0]
+                # TODO: dependency_parsing
+                if args.custom_features == "words":
+                    feature_groups = extract_groups(word_ids)
+                else:
+                    raise NotImplementedError(f"Unsupported custom features: '{args.custom_features}'")
+
             if args.method_class == "lime":
-                res = method.explain_text(tup_input_pair, label=predicted_label,
-                                          num_samples=5, explanation_length=args.explanation_length)
+                res = method.explain_text(raw_input, label=predicted_label,
+                                          num_samples=5, explanation_length=args.explanation_length,
+                                          custom_features=feature_groups)
                 samples = res["samples"]
             else:
+                # For IME with external LM, sampling data is generated on the fly
+                if args.method == "ime_elm":
+                    method.prepare_data(raw_input, label=predicted_label,
+                                        pretokenized_text_data=pretok_input,
+                                        custom_features=feature_groups)
+
                 # In IME, select random sample from a random explained feature
                 perturbable_indices = \
                     torch.arange(encoded_example["input_ids"].shape[1])[encoded_example["perturbable_mask"][0]]
-                selected_feature = perturbable_indices[torch.randint(perturbable_indices.shape[0], ())].item()
+                num_groups = perturbable_indices.shape[0] if feature_groups is None else len(feature_groups)
+                if feature_groups is None:
+                    selected_feature = perturbable_indices[torch.randint(num_groups, ()).item()]
+                else:
+                    selected_feature = torch.randint(num_groups, ()).item()
 
-                # NOTE: assuming aligned vocabulary
-                # TODO: double check for higher units
+                # IMPORTANT: assuming shared vocabulary, so we do not need to convert anything to generator's repr.
                 res = method.estimate_feature_importance(idx_feature=selected_feature,
                                                          instance=encoded_example["input_ids"],
                                                          num_samples=2,
                                                          perturbable_mask=encoded_example["perturbable_mask"],
-                                                         feature_groups=None,
+                                                         feature_groups=feature_groups,
                                                          **encoded_example["aux_data"])
 
                 samples = res["samples"]
 
-            # sample[0] is by convention the original sample in LIME
+            # sample[0] is by convention the original sample in LIME, which does not use LIME's masking strategy
             idx_selected = np.random.randint(1 if args.method_class == "lime" else 0,
                                              len(samples))
             input_ids.append(samples[idx_selected])
