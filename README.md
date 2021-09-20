500c9bbd247075763468aa14a9517ba6cbaad599
Matej Klemen <matej.klemen1337@gmail.com>
2021-09-20 22:20:49 +0200

    Update highlight plot, add example in README

--- README.md
+++ README.md
@@ -45,4 +45,74 @@ print(res["importance"])
 
 ```
 
-More advanced examples will be added in the future.
+## Visualization
+
+Analyzing the (sub)word importance through plain text can be annoying, so the package offers 
+a highlight plot visualization that highlights the important units **for** the predicted label
+with green, and important units **against** the predicted label with red. 
+Continuing from the previous example, we can visualize the explanation with following code.
+
+```python3
+from explain_nlp.visualizations.highlight import highlight_plot
+
+highlight_plot([res["input"]],
+               importances=[res["importance"].tolist()],
+               pred_labels=["entailment"],
+               actual_labels=["entailment"],
+               path="my_visualization.html",
+               skip_tokens=["[PAD]"])
+```
+
+This will store the following visualization into the file `my_visualization.html`. By providing 
+`skip_tokens=["PAD"]` we are skipping the visualization for `[PAD]` tokens as those are unimportant.
+
+![Visualizing the (sub)word importance](doc/visualization_example1.png)
+
+If the explanation is computed for longer units, such as words (where a word may contain multiple subwords), 
+the argument `custom_features` must be provided, specifying how the primary units (subwords) are combined 
+into longer units (words).
+
+Let's say we compute a word-based explanation (using `WordExplainer`*) for sentence pair used above, getting 
+the following explanation.
+```python
+import torch
+
+res = {
+    "input": ["[CLS]", "a", "shirt", "less", "man", "skate", "boards", "on", "a", "ledge", ".", "[SEP]",
+              "a", "man", "without", "a", "shirt", ".", "[SEP]"],
+    "importance": torch.tensor([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                                0.01, 0.2, 0.02, 0.1, -0.05, 0.01, 0.05, 0.001,
+                                0.01, 0.05, 0.02, 0.01, -0.1, 0.01]),
+    "custom_features": [[1], [2, 3], [4], [5, 6], [7], [8], [9], [10],
+                        [12], [13], [14], [15], [16], [17]]
+}
+```
+
+The explanation is similar to the previous one, but now the importance is provided for words, which are 
+sometimes made up of multiple subwords. The conversion is provided in `res["custom_features"]`, for example
+the first word (index 0) inside custom features is made up of the token "a" (subword at index 1), 
+the second word (index 1) is made up of the tokens ["shirt", "less"] (subwords at indices 2 and 3), and so on.
+Notice that the importance of custom features is stored after importance of primary units (in this case after 
+19 zeroes).
+
+To create the visualization, they custom feature groups need to be provided with `custom_features`. 
+```python3
+from explain_nlp.visualizations.highlight import highlight_plot
+
+highlight_plot([res["input"]],
+               importances=[res["importance"].tolist()],
+               pred_labels=["entailment"],
+               actual_labels=["entailment"],
+               custom_features=[res["custom_features"]],
+               path="my_visualization.html",
+               skip_tokens=["[PAD]"])
+```
+
+![Visualizing the word importance](doc/visualization_example2.png)
+
+The numbers in front of words (e.g., `#20`) are IDs that allow matching longer units in case they are made up of units that
+are scattered throughout the text.
+
+
+`*` More advanced examples will be added in the future.
--- explain_nlp/visualizations/highlight.py
+++ explain_nlp/visualizations/highlight.py
@@ -12,8 +12,6 @@ def base_visualization(body_html: str, head_js="", body_js="", path=None):
     call_path = __file__.split(os.path.sep)[:-1]
     with open(os.path.join(os.path.sep.join(call_path), "highlight.css"), "r") as f:
         css_properties = f.read()
-    with open(os.path.join(os.path.sep.join(call_path), "highlight.js"), "r") as f:
-        js_code = f.read()
 
     visualization = \
     f"""
@@ -33,9 +31,6 @@ def base_visualization(body_html: str, head_js="", body_js="", path=None):
             {body_html}
             </div>
         </div>
-        <script type="text/javascript">
-        {js_code}
-        </script>
         <script type="text/javascript">
         {body_js}
         </script>
@@ -62,7 +57,9 @@ def highlight_plot(sequences: List[List[str]],
                    pred_labels: List,
                    actual_labels: Optional[List] = None,
                    custom_features: Optional[List[List[List[int]]]] = None,  # list of feature groups per example
-                   path: Optional[str] = None):
+                   path: Optional[str] = None,
+                   skip_tokens: List[str] = None):
+    eff_skip_tokens = {"[PAD]"} if skip_tokens is None else set(skip_tokens)
     eff_actual_labels = [None for _ in range(len(sequences))] if actual_labels is None else actual_labels
     eff_custom_features = [[] for _ in range(len(sequences))] if custom_features is None else custom_features
 
@@ -127,41 +124,69 @@ def highlight_plot(sequences: List[List[str]],
 
         curr_ex_html = []
         # If label is not given, color it with yellow, indicating "unknown correctness"
-        label_color = "rgb(238, 232, 170)"
+        label_correctness_color = "rgb(252, 221, 136)"
         eff_actual_label = "/"
         if curr_actual_label is not None:
-            label_color = "rgba(200, 247, 197, 1)" if curr_actual_label == curr_pred_label else "rgba(236, 100, 75, 1)"
+            label_correctness_color = "rgb(141, 199, 105)" if curr_actual_label == curr_pred_label \
+                else "rgb(255, 176, 176)"
             eff_actual_label = curr_actual_label
 
-        curr_ex_html.append(f"<span class='example-label' "
-                            f"title='Actual label: {eff_actual_label}' "
-                            f"style='background-color: {label_color}'> "
-                            f"Predicted label: {curr_pred_label} "
-                            f"</span>")
-        curr_ex_html.append("<br />")
+        curr_ex_html.append(f"<div class='example-label' "
+                            f"title='Actual label: {eff_actual_label}'> "
+                            f"Explanation for: "
+                            f"<div class='square' style='background-color: {label_correctness_color}'></div>"
+                            f"<strong> {curr_pred_label}</strong>"
+                            f"</div>")
 
+        is_open = False
         for idx_tok, curr_tok in enumerate(curr_seq):
+            if curr_tok in eff_skip_tokens:
+                continue
+
             unscaled_imp = np_importances[i][token_to_importance[idx_tok]]
             scaled_imp = curr_sc_imps[token_to_importance[idx_tok]]
             # Most green and most red colors indicate highest and (-highest) importances (i.e. equal tails)
-            curr_color = f"rgba(0, 153, 0, {scaled_imp})" if scaled_imp > 0 else f"rgba(255, 0, 0, {abs(scaled_imp)})"
+            if -1e-5 < unscaled_imp < 1e-5:
+                highlight_color = "rgb(255, 255, 255)"
+                outline_color = "rgb(255, 255, 255)"
+            else:
+                highlight_color = f"rgba(135, 220, 61, {scaled_imp})" if scaled_imp > 0 else f"rgba(212, 68, 68, {abs(scaled_imp)})"
+                outline_color = f"rgb(55, 136, 5)" if scaled_imp > 0 else f"rgb(204, 0, 0)"
 
             tok_parts = []
             if is_start[idx_tok]:
+                tok_parts.append(f"<div style='display: inline-flex; flex-direction: column;'>")
+                tok_parts.append(f"<span class='row' style='font-size: 8pt; padding: 0; margin: 0; text-align: center; line-height: 10px'>({unscaled_imp:.3f})</span>")
                 tok_parts.append(f"<span class='example-unit' "
-                                 f"title='{unscaled_imp: .4f}' "
-                                 f"style='background-color: {curr_color}'"
-                                 f"data-sequence='{i}' data-feature='{token_to_importance[idx_tok]}' "
-                                 f"onmouseenter='toggleMarked(this)' "
-                                 f"onmouseleave='toggleMarked(this)'>")
+                                 f"title='{unscaled_imp:.4f}' "
+                                 f"style='background-color: {highlight_color}; border-width: 2px; border-style: solid; border-color: {outline_color}' "
+                                 f"data-sequence='{i}' data-feature='{token_to_importance[idx_tok]}'>")
                 # Display an ID in front of (possibly scattered) units making up large, discontiguous unit
                 if is_multiunit[idx_tok]:
-                    tok_parts.append(f"<span class='unit-id'>#{token_to_importance[idx_tok]}</span>")
+                    tok_parts.append(f"<span class='unit-id'"
+                                     f"style='background-color: {outline_color}; border-width: 2px; border-style: solid; border-color: {outline_color}; user-select: none;'>"
+                                     f"#{token_to_importance[idx_tok]}"
+                                     f"</span>")
 
-            tok_parts.append(html.escape(str(curr_tok)))
+                is_open = True
+
+            if is_open:
+                tok_parts.append(f" {html.escape(str(curr_tok))}")
+            else:
+                # Hack to handle units that are not provided in custom features and also not skipped
+                # (e.g., special tokens like [CLS])
+                tok_parts.append(f"<div style='display: inline-flex; flex-direction: column;'>")
+                tok_parts.append(f"<span class='row' style='font-size: 8pt; padding: 0; margin: 0; text-align: center; line-height: 10px'>({0.000:.3f})</span>")
+                tok_parts.append(f"<span class='example-unit' "
+                                 f"title='{0.0000:.4f}' "
+                                 f"style='background-color: rgb(255, 255, 255); border-width: 2px; border-style: solid; border-color: rgb(255, 255, 255)' "
+                                 f"data-sequence='{i}' data-feature='0.0'>  {html.escape(str(curr_tok))} </span>"
+                                 f"</span></div>")
 
             if is_end[idx_tok]:
                 tok_parts.append(f"</span>")
+                tok_parts.append(f"</span></div>")
+                is_open = False
 
             curr_ex_html.append("".join(tok_parts))
 
@@ -172,13 +197,10 @@ def highlight_plot(sequences: List[List[str]],
 
 
 if __name__ == "__main__":
-    highlight_plot(sequences=[["The", "show", "was", "fucking", "shite", "but", "I", "liked", "the", "end"],
-                              ["The", "big", "brown", "fox", "jumps", "over", "a", "lazy", "dog"]],
-                   importances=[[0.0, -0.05, 0.0, 0.35, 0.05, 0.01, 0.0, -0.1, 0.0, -0.05, 0.45, -0.2],
-                                [0.02, 0.2, 0.1, 0.1, 0.05, 0.05, 0.01, -0.1, 0.1, 0.3, -0.15]],
-                   pred_labels=["toxic", "clean"],
-                   actual_labels=["toxic", None],
-                   custom_features=[[[3, 4, 9], [7]],
-                                    [[1, 2, 3], [7, 8]]],
+    highlight_plot(sequences=[["The", "big", "brown", "fox", "jumps", "over", "a", "lazy", "dog"]],
+                   importances=[[0.02, 0.2, 0.1, 0.1, 0.05, 0.05, 0.01, -0.1, 0.1,
+                                 0.001, 0.2, 0.1, 0.05, 0.05, -0.1]],
+                   pred_labels=["clean"],
+                   actual_labels=["clean"],
+                   custom_features=[[[0], [1, 2, 3, 7], [4], [5], [6], [8]]],
                    path="tmp_refactor.html")
-
